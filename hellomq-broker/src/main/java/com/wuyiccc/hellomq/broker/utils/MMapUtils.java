2219f5cd025caa55d19efd36b05d43e6c401469e
wuyiccc <3406324191@qq.com>
2024-08-25 15:55:41 +0800

    feat: mmap内存分配与释放

---
+++ hellomq-broker/src/main/java/com/wuyiccc/hellomq/broker/utils/MMapUtils.java
@@ -0,0 +1,159 @@
+package com.wuyiccc.hellomq.broker.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.lang.reflect.Method;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+import java.sql.Time;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author wuyiccc
+ * @date 2024/8/25 11:01
+ * // 支持基于java的MMap api访问文件的能力 (文件的读写)
+ * // 支持指定的offset的文件映射 (end offset - start offset = 映射的内存体积)
+ * // 文件从指定的offset开始读取
+ * // 文件从指定的offset开始写入
+ * // 文件映射后的内存释放
+ */
+public class MMapUtils {
+
+    private File file;
+
+    private MappedByteBuffer mappedByteBuffer;
+
+    private FileChannel fileChannel;
+
+
+    /**
+     * 指定offset做文件的映射
+     *
+     * @param filePath    文件路径
+     * @param startOffset 开始映射的offset
+     * @param mappedSize  映射的体积
+     */
+    public void loadFileInMMap(String filePath, int startOffset, int mappedSize) throws IOException {
+
+        this.file = new File(filePath);
+        if (!file.exists()) {
+            throw new FileNotFoundException("filePath is " + filePath + " inValid");
+        }
+
+        this.fileChannel = new RandomAccessFile(file, "rw").getChannel();
+
+        this.mappedByteBuffer = this.fileChannel.map(FileChannel.MapMode.READ_WRITE, startOffset, mappedSize);
+        System.out.println("--------------");
+    }
+
+
+    /**
+     * 支持从文件的指定offset开始读取内容
+     */
+    public byte[] readContent(int readOffset, int size) {
+
+        this.mappedByteBuffer.position(readOffset);
+        byte[] content = new byte[size];
+
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            // 这里是从内存中访问
+            byte b = this.mappedByteBuffer.get(readOffset + i);
+            content[j++] = b;
+        }
+
+        return content;
+    }
+
+    /**
+     * 默认不强制刷盘
+     */
+    public void writeContent(byte[] content) {
+        this.writeContent(content, false);
+    }
+
+    /**
+     * 写入数据到磁盘中
+     *
+     * @param content 数据内容
+     * @param force   是否强制刷盘
+     */
+    public void writeContent(byte[] content, boolean force) {
+        // 默认刷到pageCache中
+        // 如果需要强制刷盘, 这里要兼容
+        this.mappedByteBuffer.put(content);
+        if (force) {
+            this.mappedByteBuffer.force();
+        }
+    }
+
+
+    /**
+     * 释放DirectByteBuffer的mapped直接内存空间, 可以通过arthas来观察maaped内存释放释放, 参考rocketmq的MappedFile#clean()方法
+     */
+    public void clear() {
+
+        if (Objects.isNull(this.mappedByteBuffer) || !this.mappedByteBuffer.isDirect() || this.mappedByteBuffer.capacity() == 0) {
+            return;
+        }
+        invoke(invoke(viewed(this.mappedByteBuffer), "cleaner"), "clean");
+    }
+
+    private static Object invoke(final Object target, final String methodName, final Class<?>... args) {
+        return AccessController.doPrivileged((PrivilegedAction<Object>) () -> {
+            try {
+                Method method = method(target, methodName, args);
+                method.setAccessible(true);
+                return method.invoke(target);
+            } catch (Exception e) {
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
+    private static Method method(Object target, String methodName, Class<?>[] args) throws NoSuchMethodException {
+
+        try {
+            return target.getClass().getMethod(methodName, args);
+        } catch (NoSuchMethodException e) {
+            return target.getClass().getDeclaredMethod(methodName, args);
+        }
+    }
+
+    private static ByteBuffer viewed(ByteBuffer buffer) {
+        String methodName = "viewedBuffer";
+        Method[] methods = buffer.getClass().getMethods();
+        for (int i = 0; i < methods.length; i++) {
+            if (methods[i].getName().equals("attachment")) {
+                methodName = "attachment";
+                break;
+            }
+        }
+
+        ByteBuffer viewedBuffer = (ByteBuffer) invoke(buffer, methodName);
+        if (Objects.isNull(viewedBuffer)) {
+            return buffer;
+        } else {
+            return viewed(viewedBuffer);
+        }
+    }
+
+
+    //public static void main(String[] args) throws IOException, InterruptedException {
+    //    MMapUtils mMapUtils = new MMapUtils();
+    //
+    //    mMapUtils.loadFileInMMap("./doc/test.txt", 0, 1 * 1024 * 1024);
+    //
+    //    mMapUtils.clear();
+    //
+    //
+    //}
+
+
+}
